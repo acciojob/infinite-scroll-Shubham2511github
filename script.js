0d754ef4477e8eb0c89f4361f9b9466e75ca4410
Shubham Mane <123303343+Shubham2511github@users.noreply.github.com>
2023-06-18 23:11:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,45 @@
 //your code here!
+document.addEventListener("DOMContentLoaded", function () {
+  const list = document.getElementById("infi-list");
+  const listItemLimit = 10;
+  let currentPage = 1;
 
+  function createListItem(text) {
+    const li = document.createElement("li");
+    li.textContent = text;
+    return li;
+  }
+
+  function addListItems(start, end) {
+    for (let i = start; i <= end; i++) {
+      const listItem = createListItem("List Item " + i);
+      list.appendChild(listItem);
+    }
+  }
+
+  function isEndOfList() {
+    const listRect = list.getBoundingClientRect();
+    const lastItem = list.lastElementChild;
+    const lastItemRect = lastItem.getBoundingClientRect();
+
+    return lastItemRect.bottom <= listRect.bottom;
+  }
+
+  function loadMoreItems() {
+    const start = (currentPage - 1) * listItemLimit + 1;
+    const end = currentPage * listItemLimit;
+
+    addListItems(start, end);
+    currentPage++;
+  }
+
+  // Initial load
+  addListItems(1, listItemLimit);
+
+  // Scroll event listener
+  list.addEventListener("scroll", function () {
+    if (isEndOfList()) {
+      loadMoreItems();
+    }
+  });
+});
